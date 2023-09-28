826e67ae14c53fe04dd78f39340f2ad4e5f4bd89
cagilbuldu <132782937+cagilbuldu@users.noreply.github.com>
2023-09-28 14:42:47 +0300

    Proje dosyası ekle.

---
+++ ZaferTurizm.DataAccess/DbSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaferTurizm.DataAccess
+{
+    internal class DbSettings
+    {
+
+            public const string DbConnectionString = "Server=.\\; Database=ZaferTurizm01; Integrated Security=true;";
+
+    }
+}
